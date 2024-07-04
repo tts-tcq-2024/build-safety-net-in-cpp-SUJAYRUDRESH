c96a6c85686f8856d7bf89cfa3cea069bdf5a219
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 10:24:20 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -16,14 +16,26 @@ std::string Soundex::startSoundex(const std::string& name) const {
     return std::string(1, toupper(name[0]));
 }
 
+bool Soundex::isValidCode(char code) const {
+    return code != '0';
+}
+
+bool Soundex::isNewCode(char code, char prevCode) const {
+    return code != prevCode;
+}
+
+void Soundex::processCharacter(std::string& soundex, char code, char& prevCode) const {
+    if (isValidCode(code) && isNewCode(code, prevCode)) {
+        soundex += code;
+        prevCode = code;
+    }
+}
+
 void Soundex::generateRemainingSoundex(std::string& soundex, const std::string& name) const {
     char prevCode = getSoundexCode(name[0]);
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
-        }
+        processCharacter(soundex, code, prevCode);
     }
 }
 
