40179921c285f34a92a134b1681ec5efd595f20d
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 10:44:13 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -2,6 +2,7 @@
 #include <cctype>
 
 const char Soundex::soundexMap[26] = {
+//  A     B    C    D    E   F     G   H     I    J    K    L    M    N   O      P   Q    R    S    T    U    V   W     X   Y     Z
     '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '0', '6', '2', '3', '0', '1', '0', '2', '0', '2'
 };
 
