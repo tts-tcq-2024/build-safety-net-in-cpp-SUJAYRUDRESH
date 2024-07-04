aa223e6b04eba6bf63b35245ad181a9d250f39ec
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 10:05:59 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -9,13 +9,6 @@ char getSoundexCode(char c);
 // Function to generate the Soundex code for a given name.
 std::string generateSoundex(const std::string& name);
 
-// Function to get the initial character of the Soundex code.
-std::string getInitialSoundex(const std::string& name);
 
-// Function to process the remaining characters of the name to generate the Soundex code.
-std::string processRemainingCharacters(const std::string& name, const std::string& initialSoundex);
-
-// Function to pad the Soundex code to ensure it has exactly four characters.
-std::string padSoundex(const std::string& soundex);
 
 #endif // SOUNDEX_H
