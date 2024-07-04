c2f4ee3f49278a61f051434a476040394631a12e
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 13:16:06 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -6,42 +6,22 @@
 class Soundex {
 public:
     Soundex();
-
-    // Generates the Soundex code for a given name
     std::string generateSoundex(const std::string& name);
 
 private:
-    // Maps characters to their Soundex codes
     static const char soundexMap[26];
-
-    // Returns the Soundex code for a given character
+    
     char getSoundexCode(char c) const;
-
-    // Returns the initial character for Soundex encoding
     std::string startSoundex(const std::string& name) const;
-
-    // Generates the remaining Soundex codes for the name
     void generateRemainingSoundex(std::string& soundex, const std::string& name) const;
-
-    // Checks if a Soundex code is valid (not '0')
-    bool isValidSoundexCode(char code) const;
-
-    // Checks if a new Soundex code is different from the previous one
-    bool isNewCode(char code, char prevCode) const;
-
-    // Checks if a Soundex code should be added based on separation rules
-    bool shouldAddCode(char prevPrevChar, char currentChar) const;
-
-    // Checks if a character is a vowel
-    bool isVowel(char c) const;
-
-    // Pads the Soundex code with zeros to ensure it's 4 characters long
-    void padWithZeros(std::string& soundex) const;
-
-    // Checks if a character is separated by 'h' or 'w' from the current character
-    bool isSeparatedByHorW(char prevPrevCode) const;
-
+    void processCurrentChar(std::string& soundex, char currentChar, char& prevCode, char& prevPrevCode) const;
     void processCharacter(std::string& soundex, char code, char& prevCode) const;
+    bool isValidSoundexCode(char code) const;
+    bool isNewCode(char code, char prevCode) const;
+    bool shouldAddCode(char prevPrevCode, char currentChar) const;
+    bool isSeparatedByHorW(char prevPrevCode) const;
+    void padWithZeros(std::string& soundex) const;
+    bool isVowel(char c) const;
 };
 
 #endif // SOUNDEX_H
