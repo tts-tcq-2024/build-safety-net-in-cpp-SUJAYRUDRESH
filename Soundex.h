2dac891657e88eb90e413159611668a6da573ad5
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 12:07:55 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -6,19 +6,40 @@
 class Soundex {
 public:
     Soundex();
+
+    // Generates the Soundex code for a given name
     std::string generateSoundex(const std::string& name);
 
 private:
+    // Maps characters to their Soundex codes
     static const char soundexMap[26];
 
+    // Returns the Soundex code for a given character
     char getSoundexCode(char c) const;
+
+    // Returns the initial character for Soundex encoding
     std::string startSoundex(const std::string& name) const;
+
+    // Generates the remaining Soundex codes for the name
     void generateRemainingSoundex(std::string& soundex, const std::string& name) const;
-    void padWithZeros(std::string& soundex) const;
-    bool isVowel(char c) const;
-    bool isValidCode(char code) const;
+
+    // Checks if a Soundex code is valid (not '0')
+    bool isValidSoundexCode(char code) const;
+
+    // Checks if a new Soundex code is different from the previous one
     bool isNewCode(char code, char prevCode) const;
-    void processCharacter(std::string& soundex, char code, char& prevCode, char prevPrevCode) const;
-}; 
+
+    // Checks if a Soundex code should be added based on separation rules
+    bool shouldAddCode(char prevPrevChar, char currentChar) const;
+
+    // Checks if a character is a vowel
+    bool isVowel(char c) const;
+
+    // Pads the Soundex code with zeros to ensure it's 4 characters long
+    void padWithZeros(std::string& soundex) const;
+
+    // Checks if a character is separated by 'h' or 'w' from the current character
+    bool isSeparatedByHOrW(char prevChar, char currentChar) const;
+};
 
 #endif // SOUNDEX_H
