184991a65e0de0b7f736985ae186e02f5cde8db2
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 11:26:48 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -26,17 +26,35 @@ bool Soundex::isNewCode(char code, char prevCode) const {
 }
 
 void Soundex::processCharacter(std::string& soundex, char code, char& prevCode) const {
-    if (isValidCode(code) && isNewCode(code, prevCode)) {
-        soundex += code;
-        prevCode = code;
+if (isValidCode(code)) {
+        // Check if current and previous codes are the same
+        if (isNewCode(code, prevCode)) {
+            // Check if separated by 'h' or 'w'
+            if ((prevPrevCode == 'H' || prevPrevCode == 'W') && !isVowel(prevPrevCode)) {
+                // Only add the code if it's different from the previous code
+                if (code != prevCode) {
+                    soundex += code;
+                    prevCode = code;
+                }
+            } else {
+                // Always add the code if it's different from the previous code
+                soundex += code;
+                prevCode = code;
+            }
+        }
     }
 }
 
 void Soundex::generateRemainingSoundex(std::string& soundex, const std::string& name) const {
+    if (name.length() <= 1) return;
+
     char prevCode = getSoundexCode(name[0]);
+    char prevPrevCode = toupper(name[0]);
+
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
         char code = getSoundexCode(name[i]);
-        processCharacter(soundex, code, prevCode);
+        processCharacter(soundex, code, prevCode, prevPrevCode);
+        prevPrevCode = toupper(name[i]);
     }
 }
 
@@ -44,6 +62,11 @@ void Soundex::padWithZeros(std::string& soundex) const {
     while (soundex.length() < 4) {
         soundex += '0';
     }
+}
+
+bool Soundex::isVowel(char c) const {
+    c = toupper(c);
+    return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 }
 
 std::string Soundex::generateSoundex(const std::string& name) {
