9766b521e141750f2116df7be3a1b103b9dc4820
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 23:45:54 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -38,8 +38,25 @@ TEST_F(SoundexTest, CaseInsensitivity) {
     EXPECT_EQ(soundex.generateSoundex("Rudresh"), soundex.generateSoundex("rudresh"));
 }
 
+TEST_F(SoundexTest, GenerateSoundexNamesWithOnlyVowels) {
+    EXPECT_EQ(soundex.generateSoundex("Ae"), "A000");
+    EXPECT_EQ(soundex.generateSoundex("Io"), "I000");
+}
+TEST_F(SoundexTest, GenerateSoundexVeryShortNames) {
+    EXPECT_EQ(soundex.generateSoundex("A"), "A000");
+    EXPECT_EQ(soundex.generateSoundex("B"), "B000");
+}
+
+TEST_F(SoundexTest, GenerateSoundexNamesWithConsecutiveConsonants) {
+    EXPECT_EQ(soundex.generateSoundex("Bbbb"), "B100");
+    EXPECT_EQ(soundex.generateSoundex("Ddd"), "D300");
+}
+
 TEST_F(SoundexTest, NonAlphabeticCharacters) {
     EXPECT_EQ(soundex.generateSoundex("Sujay-s"), "S220");
+}
+TEST_F(SoundexTest, GenerateSoundexEmptyName) {
+    EXPECT_EQ(soundex.generateSoundex(""), "");
 }
 
 int main(int argc, char **argv) {
