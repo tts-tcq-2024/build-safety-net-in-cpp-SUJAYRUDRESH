203aebc5195903a80f81276d5ec6078b3c4c611f
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-04 13:45:21 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -10,15 +10,15 @@ public:
 
 private:
     static const char soundexMap[26];
-    
+
     char getSoundexCode(char c) const;
     std::string startSoundex(const std::string& name) const;
     void generateRemainingSoundex(std::string& soundex, const std::string& name) const;
     void processCurrentChar(std::string& soundex, char currentChar, char& prevCode, char& prevPrevCode) const;
+    bool shouldProcessCharacter(char currentCode, char prevCode, char prevPrevCode) const;
     void processCharacter(std::string& soundex, char code, char& prevCode) const;
     bool isValidSoundexCode(char code) const;
     bool isNewCode(char code, char prevCode) const;
-    bool shouldAddCode(char prevPrevCode, char currentChar) const;
     bool isSeparatedByHorW(char prevPrevCode) const;
     void padWithZeros(std::string& soundex) const;
     bool isVowel(char c) const;
